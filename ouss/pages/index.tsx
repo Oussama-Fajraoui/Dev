044abb4c8eedbfa0f7ffe6032c9da5da5a4f9d20
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-10-08 21:54:37 +0100

    header

---
+++ ouss/pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Header from '../components/Header'
+
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>  
+      </Head>
+      {/* header */}
+      <Header />
+      {/* Hero */}
+      {/* About */}
+      {/* Experience */}
+      {/* skills */}
+      {/* Proects */}
+      {/* Contact Me */}
+    </div>
+  )
+}
+
+export default Home
